d714d8069740acadc47f54d6ef07c28498438e75
milanb0z <bozic411@gmail.com>
2023-09-06 00:58:10 +0200

    Db Connect

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-
 require("dotenv").config();
+require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 
 //Router
